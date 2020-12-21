c7c90d0f8d1cd1e41b1d4b2645a3ab0f23af003e
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-21 17:16:40 +0000

    Removed class from Logo as it is no longer required, removed use of unordered list as all links are managed via css

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -6,13 +6,11 @@ import Logo from './Logo'
 export default function Nav() {
   return (
     <nav className={navStyles.nav}>
-      <ul>
-        <Link className={navStyles.navLink} to="/">HOME</Link>
-        <Link className={navStyles.navLink} to="/locations/">LOCATIONS</Link>
-        <Logo className={navStyles.navLink} />
-        <Link className={navStyles.navLink} to="/menu/">MENU</Link>
-        <Link className={navStyles.navLink} to="/order/">ORDER</Link>
-      </ul>
+      <Link className={navStyles.navLink} to="/">HOME</Link>
+      <Link className={navStyles.navLink} to="/locations/">LOCATIONS</Link>
+      <Logo />
+      <Link className={navStyles.navLink} to="/menu/">MENU</Link>
+      <Link className={navStyles.navLink} to="/order/">ORDER</Link>
     </nav>
   )
 }
