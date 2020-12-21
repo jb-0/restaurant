2fd889ca2b32d4bee22ba9f1f3de846a2ca94d6d
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-21 19:52:50 +0000

    Made navbar collapse on scroll as well as shrink logo, both with transitions

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import navStyles from './Nav.module.css';
 import Logo from './Logo'
 
 export default function Nav() {
+  const [scrollPosition, setScrollPosition] = useState();
+
+  const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+  };
+
+  useEffect(() => {
+      window.addEventListener('scroll', handleScroll, { passive: true });
+
+      return () => {
+        console.log('cleanup');
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
+
   return (
-    <nav className={navStyles.nav}>
+    <nav className={`${navStyles.nav} ${scrollPosition > 60 ? navStyles.navCollapse : null}`}>
       <Link className={navStyles.navLink} to="/menu/">MENU</Link>
       <Link className={navStyles.navLink} to="/order/">ORDER</Link>
-      <Logo />
+      <span className={navStyles.navLogo}><Logo /></span>
       <Link className={navStyles.navLink} to="/locations/">LOCATIONS</Link>
       <Link className={navStyles.navLink} to="/">RECIPES</Link>
     </nav>
