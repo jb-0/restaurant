8ac7f49d1f7717193158ec203e9eea435d99c727
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-22 10:07:22 +0000

    Handled contentful content including rich text, image yet to be included and further styling required

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,41 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/common/Layout'
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <Layout>
-      <h1>Hello</h1>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      <p>
-        Some thing happened. Some thing happened. Some thing happened. Some
-        thing happened.
-      </p>
-      
+      {data.allContentfulHome.nodes.map(section => {
+        return (
+          <div key={section} className="section">
+            <h1>{section.heading}</h1>
+            <h2>{section.subHeading}</h2>
+            <p>{renderRichText(section.detail)}</p>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allContentfulHome {
+      nodes {
+        detail {
+          raw
+        }
+        heading
+        subHeading
+        image {
+          file {
+            url
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
