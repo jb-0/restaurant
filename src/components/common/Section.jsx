d9c3930b9e62709ec526b5fe7d15230cb40687f1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-23 10:14:44 +0000

    Conditionally render elements based on whether they exist in CRM, added flex styling for a basic layout of image to left of text

--- src/components/common/Section.jsx
+++ src/components/common/Section.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import sectionStyles from './Section.module.css'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer' 
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 export default function Section(props) {
   return (
-    <div key={props.node.section} className={sectionStyles.section}>
-      <div>
-        <img className={sectionStyles.sectionImage} src={props.node.image.file.url} alt={props.node.image.description} />
-        <h1>{props.node.heading}</h1>
-        <h2>{props.node.subHeading}</h2>
-        {documentToReactComponents(JSON.parse(props.node.detail.raw))}
-        <hr></hr>
+    <>
+      <div key={props.node.section} className={sectionStyles.section}>
+        {
+          /* If an image exists then render it */
+          props.node.image ? (
+            <div className={sectionStyles.subSection}>
+              <img
+                className={`${sectionStyles.sectionImage}`}
+                src={props.node.image.file.url}
+                alt={props.node.image.description}
+              />
+            </div>
+          ) : null
+        }
+        <div className={sectionStyles.subSection}>
+          {/* To allow full flexibility all fields are conditional so they are not mandated */}
+          <h1>{props.node.heading ? props.node.heading : null}</h1>
+          <h2>{props.node.subHeading ? props.node.subHeading : null}</h2>
+          {props.node.detail.raw
+            ? documentToReactComponents(JSON.parse(props.node.detail.raw))
+            : null}
+        </div>
       </div>
-    </div>
+      <hr></hr>
+    </>
   )
 }
