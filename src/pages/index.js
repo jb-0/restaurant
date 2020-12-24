c0c655ba36ed9dcc39eb570dbb02aa69df414fd8
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-24 12:39:18 +0000

    Updated index page to use uuid as unique key

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/common/Layout'
 import Section from '../components/common/Section'
+import { v4 as uuidv4 } from 'uuid'
 
 const IndexPage = ({ data }) => {
   console.log(data)
   return (
     <Layout>
       {data.allContentfulSection.nodes.map(node => {
-        return <Section key={node.section} node={node} />
+        return <Section key={uuidv4()} node={node} />
       })}
     </Layout>
   )
