86be82b29b0e1c1bc185271f640c8f8814e63c6c
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-27 17:22:13 +0000

    Queried blog listing and present on blog page using blog card element

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,13 +1,31 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/common/Layout'
+import BlogCard from '../components/blog/BlogCard'
 
-const RecipePage = () => {
+const RecipePage = ({ data }) => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>A place for recipes, information and whatever else! No blogs posted yet.</p>
+      <p>A place for recipes, information and whatever else! See below for a list of blogs.</p>
+      {data.allContentfulBlog.nodes.map(node => {
+        return <BlogCard blog={node}/>
+      })}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allContentfulBlog(sort: { fields: [createdAt], order: DESC }) {
+      nodes {
+        author
+        createdAt(formatString: "HH:mm:ss DD MMMM YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
 
 export default RecipePage
