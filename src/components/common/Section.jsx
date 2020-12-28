cd4570d41a53a99b0d822b4e68480df06456c68d
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-28 17:47:45 +0000

    Refactored to use destructuring to improve code readability

--- src/components/common/Section.jsx
+++ src/components/common/Section.jsx
@@ -5,8 +5,15 @@ import { MARKS } from '@contentful/rich-text-types'
 import Menu from '../menu/Menu'
 
 export default function Section(props) {
-  // Given length of object ref, assign to a const to simplify referencing later
-  const pageProps = props.node.childContentfulSectionSectionItemsJsonNode
+  /* Destructure props to get the relevant variables */
+  const {
+    childContentfulSectionSectionItemsJsonNode: pageProps,
+    detail,
+    heading,
+    image,
+    section,
+    subHeading,
+  } = props.node;
 
   const options = {
     renderMark: {
@@ -16,35 +23,34 @@ export default function Section(props) {
 
   return (
     <>
-      <div key={props.node.section} className={sectionStyles.section}>
+      <div key={section} className={sectionStyles.section}>
         {
           /* If an image exists then render it */
-          props.node.image ? (
+          image ? (
             <div className={sectionStyles.subSection}>
               <img
-                src={props.node.image.file.url}
-                alt={props.node.image.description}
+                src={image.file.url}
+                alt={image.description}
               />
             </div>
           ) : null
         }
         <div className={sectionStyles.subSection}>
           {/* To allow full flexibility all fields are conditional so they are not mandated */}
-          <h1>{props.node.heading ? props.node.heading : null}</h1>
-          <h2>{props.node.subHeading ? props.node.subHeading : null}</h2>
-          {props.node.detail
+          <h1>{heading ? heading : null}</h1>
+          <h2>{subHeading ? subHeading : null}</h2>
+          {detail
             ? documentToReactComponents(
-                JSON.parse(props.node.detail.raw),
+                JSON.parse(detail.raw),
                 options
               )
             : null}
 
           {/* MENU specific handling, expectation is that items are provided however still using
           conditional rendering just as a defensive measure, in case input is erroneous for e.g. */}
-          {pageProps.targetPage === 'menu' && pageProps.items ?
-          <Menu items={JSON.parse(pageProps.items)} />
-          : null}
-
+          {pageProps.targetPage === 'menu' && pageProps.items ? (
+            <Menu items={JSON.parse(pageProps.items)} />
+          ) : null}
         </div>
       </div>
       <hr></hr>
